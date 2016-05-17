ae85bd1cedbdbedca21ca2ca3e3437fcfebbde94
GaoChengliang <gaochengliang@pku.edu.cn>
2016-05-17 21:45:14 +0800

    ticket: demo ui finished (#4)
    demo ui finished

---
+++ FinancialAid/FillFormWebViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FillFormWebViewController.swift
+//  FinancialAid
+//
+//  Created by GaoChengliang on 16/5/17.
+//  Copyright © 2016年 pku. All rights reserved.
+//
+
+import UIKit
+
+class FillFormWebViewController: UIViewController, UIWebViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        webView.delegate = self
+        webView.loadRequest(NSURLRequest(URL: NSURL(string: "https://www.baidu.com")!))
+        if (true) {
+            print(1)
+        }
+    }
+
+    @IBOutlet weak var webView: UIWebView!
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
