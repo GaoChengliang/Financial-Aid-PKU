45961e38ea32c577855a0a378e4d2768b043e7c7
Peng Zhao <zhaopeng19911109@gmail.com>
2016-05-17 15:39:43 +0800

    no features. framework added (#2)
    no features. framework added

---
+++ FinancialAid/TabBarController.swift
@@ -0,0 +1,13 @@
+//
+//  TabBarController.swift
+//  FinancialAid
+//
+//  Created by PengZhao on 16/5/17.
+//  Copyright © 2016年 pku. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+}
