115ebc5c5f6fc492eb9524d745dc130ecd4ce2bf
GaoChengliang <gaochengliang@pku.edu.cn>
2016-07-10 09:07:53 +0800

    welcome page time

--- FinancialAid/AppDelegate.swift
+++ FinancialAid/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication,
          didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?)
             -> Bool {
-        sleep(3)
+        sleep(2)
         UIApplication.sharedApplication().setMinimumBackgroundFetchInterval(
             UIApplicationBackgroundFetchIntervalMinimum)
 
