8f42a7c09ca440e8612b9c6db544cca2e3de5e2d
GaoChengliang <gaochengliang@pku.edu.cn>
2016-09-27 17:52:06 +0800

    remove all pods

--- Pods/Target Support Files/SDCycleScrollView/SDCycleScrollView-umbrella.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "SDCollectionViewCell.h"
-#import "SDCycleScrollView.h"
-#import "UIView+SDExtension.h"
-
-FOUNDATION_EXPORT double SDCycleScrollViewVersionNumber;
-FOUNDATION_EXPORT const unsigned char SDCycleScrollViewVersionString[];
-
